8243d03937f545186bac90659498a58761fbbf54
duddavenu15 <31390494+duddavenu15@users.noreply.github.com>
2021-06-18 18:55:21 +0800

    powershell script for log filter

---
+++ logparser.ps1
@@ -0,0 +1,7 @@
+﻿$logfile = "C:\Users\duddav\Desktop\log.txt"
+Get-Content $logfile | Where-Object {$_ -like "*ERROR*"} | ForEach-Object {
+$ip = $_.ToString().Split()[0]
+$msg = $_.ToString().Split()[2]
+Write-Output "$ip  $msg"
+
+}
